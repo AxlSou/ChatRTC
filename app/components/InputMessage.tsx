d8adaa8ac5e1dd9e494755d40ccf593a445af9f5
AxlSou <axlsoubielle@gmail.com>
2023-03-04 20:04:58 +0000

    Send messages feature added

---
+++ app/components/InputMessage.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useState } from 'react'
+import { useSupabase } from './supabase-provider'
+
+export default function InputMessage () {
+  const [text, setText] = useState('')
+  const { supabase } = useSupabase()
+
+  const handleSubmit = async () => {
+    const { error } = await supabase.from('messages').insert({ content: text })
+    if (error) {
+      throw new Error(String(error))
+    }
+  }
+
+  return (
+    <form className='bg-green-300 p-2 py-3 grid grid-cols-[60px,_1fr,60px] gap-x-2' onSubmit={handleSubmit}>
+      <input
+        className='col-start-2 px-4 rounded-md focus:outline-none'
+        type='text'
+        placeholder='Write your message here...'
+        onChange={(e) => setText(e.currentTarget.value)}
+      />
+      <button className='bg-white rounded-md' type='submit'>Send</button>
+    </form>
+  )
+}
