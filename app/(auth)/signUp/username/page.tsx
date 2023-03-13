903ff6002227b8fec9e9e3fcaaf9920cf7e78355
AxlSou <axlsoubielle@gmail.com>
2023-03-13 20:34:42 +0000

    Create username site created

---
+++ app/(auth)/signUp/username/page.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { useSupabase } from '@/app/components/supabase-provider'
+import { useState } from 'react'
+import { redirect } from 'next/navigation'
+
+export default async function CreateUserName () {
+  const [username, setUsername] = useState('')
+  const { supabase, session } = useSupabase()
+
+  if (session) {
+    const checkUsername = await supabase.from('users').select('username').eq('id', session?.user.id)
+    if (checkUsername) {
+      redirect('/home')
+    }
+  } else {
+    redirect('/signUp')
+  }
+
+  const insertUsername = async () => {
+    const { error } = await supabase.from('users').update({ Username: username }).eq('id', session?.user.id)
+    if (error) console.log(error)
+  }
+
+  return (
+    <main>
+      <div className='container h-screen grid place-content-center'>
+        <form className=' w-fit h-auto p-12 bg-secondary rounded-md shadow-md' onSubmit={insertUsername}>
+          <h1 className='text-3xl font-bold text-white text-center underline mb-10'>Create your username</h1>
+          <div className='flex flex-col text-left text-white'>
+            <label className='text-lg mb-1'>Username:</label>
+            <input className='rounded-md mb-6 py-1 px-2 bg-primary focus:outline-none' type='text' onChange={(e) => setUsername(e.target.value)} />
+            <input
+              className='bg-tertiary rounded-md p-2 text-lg mt-4 mb-2 hover:cursor-pointer hover:bg-opacity-80 transition-all active:bg-opacity-60'
+              type='submit'
+              value='Create'
+            />
+          </div>
+        </form>
+      </div>
+    </main>
+  )
+}
