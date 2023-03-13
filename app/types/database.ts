eb780da987e0f53a85f928b1c7e49c3300d8ec3a
AxlSou <axlsoubielle@gmail.com>
2023-03-13 20:36:08 +0000

    Types added for users table

--- app/types/database.ts
+++ app/types/database.ts
@@ -14,19 +14,36 @@ export interface Database {
           content: string
           created_at: string | null
           id: string
-          user_id: string | null
+          user_id: string
         }
         Insert: {
           content: string
           created_at?: string | null
           id?: string
-          user_id?: string | null
+          user_id?: string
         }
         Update: {
           content?: string
           created_at?: string | null
           id?: string
-          user_id?: string | null
+          user_id?: string
+        }
+      }
+      users: {
+        Row: {
+          email: string | null
+          id: string
+          Username: string | null
+        }
+        Insert: {
+          email?: string | null
+          id: string
+          Username?: string | null
+        }
+        Update: {
+          email?: string | null
+          id?: string
+          Username?: string | null
         }
       }
     }
