c992a8f2055c97594b668063005190eb110a6ebb
AxlSou <axlsoubielle@gmail.com>
2023-04-02 16:56:35 +0100

    InputMessage moved and ConversationList added

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -5,9 +5,9 @@ import SupabaseListener from '../components/supabase-listener'
 import SupabaseProvider from '../components/supabase-provider'
 import './globals.css'
 import { createServerClient } from '../utils/supabase-server'
-import InputMessage from '../components/InputMessage'
 import SearchBar from '../components/NewConversations/NewChat'
 import SignOut from '../components/SignOut'
+import ConversationsList from '../components/Conversations/ConversationsList'
 
 export const revalidate = 0
 
@@ -35,16 +35,18 @@ export default async function RootLayout ({ children }: { children: React.ReactN
                 <SignOut />
               </header>
               <div className='bg-yellow-300 p-2 flex items-center'><SearchBar /></div>
-              <div className='bg-purple-300 p-2'>Chat List</div>
+              <div className='bg-purple-300 p-2'>
+                {/* @ts-expect-error Server Component */}
+                <ConversationsList sessionId={session?.user.id} />
+              </div>
             </div>
-            <div className='bg-blue-300 col-start-2 row-span-1 grid grid-rows-[60px,_1fr,_65px]'>
+            <div className='bg-blue-300 col-start-2 row-span-1 grid grid-rows-[60px,_1fr]'>
               <header className='bg-red-300 p-2'>
                 <h1>Name</h1>
               </header>
-              <div className='px-12'>
+              <div>
                 {children}
               </div>
-              <InputMessage />
             </div>
           </div>
         </SupabaseProvider>
