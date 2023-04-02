1e229f15e2ff28866a2b81563b3f5e57a94abd6d
AxlSou <axlsoubielle@gmail.com>
2023-04-02 16:57:06 +0100

    getSession const deleted

--- app/(site)/middleware.ts
+++ app/(site)/middleware.ts
@@ -7,12 +7,7 @@ import type { Database } from '../types/database'
 
 export async function middleware (req: NextRequest) {
   const res = NextResponse.next()
-
   const supabase = createMiddlewareSupabaseClient<Database>({ req, res })
-
-  const {
-    data: { session }
-  } = await supabase.auth.getSession()
-
+  await supabase.auth.getSession()
   return res
 }
